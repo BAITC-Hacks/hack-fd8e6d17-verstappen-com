bd890766a3847fd9c4ab2d1ed17b6e426a970c5f
Sonik018 <samalseitkasymova042@gmail.com>
2026-09-23 17:51:11 +0500

    Route business workspace to dashboard

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import TeamApp from "./team/TeamApp";
+import BusinessApp from "./business/BusinessApp";
 import { useHashRoute } from "./router";
 
 function Root() {
   const route = useHashRoute();
   if (route.startsWith("/team")) return <TeamApp />;
+  if (route.startsWith("/business")) return <BusinessApp />;
   return <App />;
 }
 
